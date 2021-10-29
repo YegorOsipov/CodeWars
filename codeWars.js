a96a543ab7f2a6a7b1aa3dd2a11cccbc5f6a9f21
Egor <gogsyan11@gmail.com>
2021-10-29 12:22:11 +0300

    7kyu 29/10/21p1

--- codeWars.js
+++ codeWars.js
@@ -174,3 +174,14 @@ function twiceAsOld(dadYearsOld, sonYearsOld) {
 function twiceAsOld(a, b) {
   return a>2*b ? a-2*b : 2*b-a;
 }
+
+
+// I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+function arrayPlusArray(arr1, arr2) {
+  let arr = arr1.concat(arr2);
+  let sum = 0;
+  for (let elem of arr) {
+    sum += elem;
+  }
+  return sum;
+}
