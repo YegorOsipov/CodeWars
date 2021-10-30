6a864e573d52686d11d256c7150b1135f086ba40
Egor <gogsyan11@gmail.com>
2021-10-30 18:07:41 +0300

    6kyu 30/10/21p1

--- codeWars.js
+++ codeWars.js
@@ -417,3 +417,10 @@ function createPhoneNumber(numbers){
   return format;
 }
 
+
+// We need a function that can transform a number into a string.
+// What ways of achieving this do you know?
+function numberToString(num) {
+  return String(num);
+}
+
