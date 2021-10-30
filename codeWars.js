0d919490220400be0d967de07729642734132330
Egor <gogsyan11@gmail.com>
2021-10-30 18:03:50 +0300

    7kyu 30/10/21p7

--- codeWars.js
+++ codeWars.js
@@ -400,3 +400,20 @@ function boolToWord( bool ){
   }
 }
 
+
+
+function createPhoneNumber(arr){
+  return '(' + String(arr[0]) + String(arr[1]) + String(arr[2]) + ')' + ' ' + String(arr[3]) + String(arr[4]) + String(arr[5]) + '-' + String(arr[6]) + String(arr[7]) + String(arr[8]) + String(arr[9]);
+}
+
+function createPhoneNumber(numbers){
+  let format = "(xxx) xxx-xxxx";
+  
+  for(let i = 0; i < numbers.length; i++)
+  {
+    format = format.replace('x', numbers[i]);
+  }
+  
+  return format;
+}
+
