1d1ade118d226048d95336097074fbf095b9268b
Egor <gogsyan11@gmail.com>
2021-10-30 21:29:11 +0300

    6kyu 30/10/21p4

--- codeWars.js
+++ codeWars.js
@@ -436,6 +436,23 @@ function even_or_odd(number) {
   if (number % 2 == 0) {
     return 'Even';
   } else {
-    return 'Odd';
+    return 'Odd'; 
   }
 }
+
+
+// Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple like so: (index1, index2).
+// For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+// The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+
+console.log(twoSum([1, 3, 4, 6], 10));
