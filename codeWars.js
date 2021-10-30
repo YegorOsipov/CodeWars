f18dccc915e73448ac4337a906fc1f911f8fce05
Egor <gogsyan11@gmail.com>
2021-10-30 21:53:46 +0300

    6kyu 30/10/21p5

--- codeWars.js
+++ codeWars.js
@@ -456,3 +456,34 @@ function twoSum(nums, target) {
 }
 
 console.log(twoSum([1, 3, 4, 6], 10));
+
+
+
+// The following was a question that I received during a technical interview for an entry level software developer position. I thought I'd post it here so that everyone could give it a go:
+// You are given an unsorted array containing all the integers from 0 to 100 inclusively. However, one number is missing. Write a function to find and return this number. What are the time and space complexities of your solution?
+
+function missingNo(nums) {
+  nums.sort(function(a, b) {
+    return a - b;
+  });
+  if (!nums.includes(0)) {
+    return 0;
+  } 
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] + 1 !== nums[i + 1]) {
+      return nums[i] + 1;
+    }
+  }
+}
+
+
+let arr1 = [9, 45, 53, 10, 100, 30, 85, 72, 69, 93, 98, 27, 73, 82, 91, 60, 5, 79, 88, 18, 71, 36, 44, 22, 89, 40, 59, 80, 81, 67, 25, 54, 13, 64, 56, 39, 48, 92, 84, 94, 87, 90, 77, 63, 32, 68, 37, 96, 23, 0, 95, 1, 52, 78, 6, 57, 50, 2, 46, 19, 76, 47, 14, 4, 3, 29, 17, 11, 21, 24, 74, 65, 12, 83, 28, 41, 66, 7, 58, 55, 51, 43, 97, 42, 86, 49, 31, 20, 75, 70, 34, 33, 38, 8, 15, 62, 35, 61, 99, 16];
+console.log(missingNo(arr1));
+missingNo =(n)=> {
+  for(i=0;i<=100;i++) {
+    if(!n.includes(i)) {
+      return i;
+    }
+  }
+}
+
