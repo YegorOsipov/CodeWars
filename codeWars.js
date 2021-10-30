87a2ed62ff88e5977178d0371c1006b3e83fdbfc
Egor <gogsyan11@gmail.com>
2021-10-30 17:40:00 +0300

    7kyu 30/10/21p6

--- codeWars.js
+++ codeWars.js
@@ -389,3 +389,14 @@ function lastDigit(n, d) {
   } 
 }
 console.log(lastDigit(123, 2));
+
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+function boolToWord( bool ){
+  if (bool == true) {
+    return 'Yes';
+  } else {
+    return 'No';
+  }
+}
+
