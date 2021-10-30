6412b963b40c416de917669f1fbaa0c751ca6354
Egor <gogsyan11@gmail.com>
2021-10-30 20:36:23 +0300

    6kyu 30/10/21p3

--- codeWars.js
+++ codeWars.js
@@ -430,3 +430,12 @@ function opposite(number) {
   return number - (number * 2);
 }
 
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+function even_or_odd(number) {
+  if (number % 2 == 0) {
+    return 'Even';
+  } else {
+    return 'Odd';
+  }
+}
