48c506d2e3f00ce125067d3ad2379ea6c1ee8e64
Egor <gogsyan11@gmail.com>
2021-10-31 12:54:04 +0300

    6kyu 31/10/21p1

--- codeWars.js
+++ codeWars.js
@@ -487,3 +487,8 @@ missingNo =(n)=> {
   }
 }
 
+
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+function greet(name){
+  return 'Hello, ' + name + ' how are you doing today?'
+}
