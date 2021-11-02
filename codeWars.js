6a23ca6b3b578d0d81cb932d454dc081985f14c3
Egor <gogsyan11@gmail.com>
2021-11-02 15:06:18 +0300

    6kyu 2/11/21p5

--- codeWars.js
+++ codeWars.js
@@ -613,3 +613,62 @@ let solution = (str) => str.split('').reverse().join('');
 function getEvenNumbers(arr){
   return arr.filter(elem => (elem % 2 == 0));
 }
+
+// let getEvenNumbers = arr => arr.filter(elem => (elem % 2 == 0));
+
+
+
+function nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMonth){
+ 
+  // Check to see you even need to save first! If not, return out
+  if(startPriceOld >= startPriceNew) {return [0, Math.round(startPriceOld - startPriceNew)];}
+ 
+  // Otherwise, declare the months and the actual total 
+  var months = 0, total = startPriceOld;
+ 
+  // while loop to run while the total is less than the new price of the car
+  while(total < startPriceNew) {
+      // increment the savings per month onto the total
+      total += savingperMonth;
+   
+      // decrease the total by the percentage loss per month
+      total -= startPriceOld * percentLossByMonth / 100;
+
+      // manipulate the start price new and old to reflect the percent loss by the current month
+      startPriceNew -= startPriceNew * percentLossByMonth / 100;
+      startPriceOld -= startPriceOld * percentLossByMonth / 100;
+       
+      // Increment the months
+      months ++;
+
+      // Use mod to determine even or odd month so that we know if we should increase the percentage loss by month by .5
+      months % 2 !== 0 ? percentLossByMonth += 0.5 : percentLossByMonth;
+   
+}
+ 
+return [months, Math.round(total - startPriceNew)];
+}
+
+function nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMonth){
+  let month = 0;
+  let total = startPriceOld;
+  if (startPriceOld >= startPriceNew) {
+    return [month, total - startPriceNew];
+  }
+  while (total <= startPriceNew) {
+    total += savingperMonth;
+    total -= startPriceOld * percentLossByMonth / 100;
+
+    startPriceNew -= startPriceNew * percentLossByMonth / 100;
+    startPriceOld -= startPriceOld * percentLossByMonth / 100;
+
+    month++;
+
+    if (month % 2 !== 0) {
+      percentLossByMonth += 0.5;
+    }
+  }
+  return [month, Math.round(total - startPriceNew)];
+}
+
+console.log(nbMonths(2000, 8000, 1000, 1.5));
