db90c43c9553e90fb960ddb1ff4db13285e89848
Egor <gogsyan11@gmail.com>
2021-11-04 13:37:38 +0300

    6kyu 4/11/21p2

--- codeWars.js
+++ codeWars.js
@@ -745,3 +745,8 @@ function spinWords(words){
 
 // Write a function that accepts two arguments and returns the remainder after dividing the larger number by the smaller number.
 const remainder = (a, b) => a > b ? a % b : b % a; 
+
+
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+const removeChar = str => str.slice(1, str.length - 1);
+console.log(removeChar('eloquent'));
